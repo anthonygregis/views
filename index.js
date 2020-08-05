875575d8b2dab33f8b71566650d571725c14b796
Anthony Gregis <anthony@thedevtron.com>
2020-08-04 19:17:36 -0500

    [Routes] Add about and blog route

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/', (req, res) => {
     res.send('Home Route')
 })
 
+app.get('/about', (req, res) => {
+    res.send('About Route')
+})
+
+app.get('/blog', (req, res) => {
+    res.send('Blog Route')
+})
+
 app.listen(8000, () => {
     console.log('Server up and running on port 8000')
 })
