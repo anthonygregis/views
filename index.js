e536d8f45adde60552846da9b2fb437f46e4be4f
Anthony Gregis <anthony@thedevtron.com>
2020-08-04 19:19:48 -0500

    [Routes] Add res.sendFile for all routes instead of using res.send

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Home Route')
+    res.sendFile(__dirname+'/views/index.html');
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Route')
+    res.sendFile(__dirname+'/views/about.html');
 })
 
 app.get('/blog', (req, res) => {
-    res.send('Blog Route')
+    res.sendFile(__dirname+'/views/blog-directory.html');
 })
 
 app.listen(8000, () => {
